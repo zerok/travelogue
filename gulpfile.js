8c7ec59994c3fc7eda1f650956e3cdefc4318dea
Horst Gutmann <zerok@zerokspot.com>
2015-04-06 18:52:50 +0200

    Cache busting custom stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var Gulp = require('gulp'),
-    Sass = require('gulp-sass');
+    Sass = require('gulp-sass'),
+    Rev = require('gulp-rev');
 
 Gulp.task('default', ['sass']);
 
 Gulp.task('sass', function() {
-    Gulp.src('static/sass/*.scss').pipe(Sass()).pipe(Gulp.dest('static/css'));
+    Gulp.src('static/sass/*.scss')
+        .pipe(Sass())
+        .pipe(Rev())
+        .pipe(Gulp.dest('static/css'))
+        .pipe(Rev.manifest())
+        .pipe(Gulp.dest('static/css'));
 });
 
 Gulp.task('watch', function() {
